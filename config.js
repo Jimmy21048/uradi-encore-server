30be3d0ecba63acaee06f84bcf0c5926cc20c594
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-05-16 09:36:47 +0300

    just another add

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 
 const conn = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    // host: process.env.HOST,
+    // user: process.env.USER,
+    // password: process.env.PASSWORD,
+    // database: process.env.DATABASE,
 
-    // host: "localhost",
-    // user: "root",
-    // database: "uradi",
-    // password: "",
-    // port: 3308
+    host: "localhost",
+    user: "root",
+    database: "uradi",
+    password: "",
+    port: 3308
 })
 
 module.exports = conn;
