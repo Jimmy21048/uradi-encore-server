a591d9976aa7fe2680ed55380b7cc884a22711f7
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-05-18 12:25:39 +0300

    cors error update 3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://uradi-encore-server.onrender.com");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
