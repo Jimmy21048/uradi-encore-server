776b05d8f3da51685a133b4eb041d02b74de2f6a
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-06-02 23:21:32 +0300

    add nodemailer

--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const conn = require('../config');
+const nodemailer = require('nodemailer');
 
 router.get('/',  (req, res) => {
     res.send("this is my page");
@@ -28,6 +29,30 @@ router.post('/', (req, res) => {
                 console.log(err);
                 return;
             }
+            let transporter = nodemailer.createTransport({
+                service: 'smtp-relay.brevo.com',
+                port: 597,
+                auth: {
+                    user: '75ea12001@smtp-brevo.com',
+                    pass: 'dDTmhn1jzS3r4xFb'
+                }
+            })
+
+            let mailOptions = {
+                from: 'uradiencore@gmail.com',
+                to: `${userData.email}`,
+                subject: 'Welcome to Uradi Encore Hotel & Resort',
+                text: `Dear ${userData.fname}, you have succesfully created an account with us. Enjoy our services. For any inquiries, contact us on 0113951657 or the email`
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if(error) {
+                    console.log(error);
+                } else {
+                    console.log('email sent' + info.response);
+                }
+
+            })
             return res.json("Sign Up successful, Login");
         })
     })
