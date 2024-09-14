550969aa17ef8deda2b5a43fc163a95c6268e5a2
Jimmy <rubiajimmy3@gmail.com>
2024-09-14 11:07:08 +0300

    add without port

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const conn = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.PORT    
+    // port: process.env.PORT    
 })
 
 module.exports = conn;
